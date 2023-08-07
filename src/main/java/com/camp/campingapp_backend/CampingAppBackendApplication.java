98ff6d2e0366b99c31f0e4c14a1e73ac3386850b
doumin <aishi1026@gmail.com>
2023-08-07 11:14:11 +0900

    first commit

---
+++ src/main/java/com/camp/campingapp_backend/CampingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.camp.campingapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camp/campingapp_backend/CampingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.campingapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
